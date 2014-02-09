33c5dc7a2159c98caa9a8763a40482aa238cf7d1
comdotlinux <comdotlinux@users.noreply.github.com>
2014-02-10 01:57:14 +0530

    Updated incompleted code + python3 fixes.
    Incorrect code committed due to previous commit.
    Also updated code to truly run on python3.

--- auth2.py
+++ auth2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import os
 import time
 import subprocess
 import base64
@@ -10,47 +9,76 @@ import hmac
 import struct
 import math
 import logging
-import ConfigParser
+import configparser
 import getpass
 
-        cf = ConfigParser.ConfigParser()
-        cf.read(sProperties)
+FORMAT = '%(asctime)-15s %(message)s '
+sProperties = 'ssh.properties'
+curTime = time.strftime("%d-%m-%Y.%H%MHrs")
 
-        LOG_FILENAME = cf.get('LOG_DETAILS', 'LOG_FILENAME')
 
-        secret = cf.get('AUTH2', 'secret')
-        key_len = cf.get('AUTH2', 'KEY_LENGTH')
-        timeWindow = cf.get('AUTH2', 'TIME_WINDOW')
-        shell = cf.get('COMMAND', 'LOGIN_SHELL')
+def get_hotp_token(secret, time_window, key_len):
+    key = base64.b32decode(secret)
+    msg = struct.pack(">Q", time_window)
+    h = hmac.new(key, msg, hashlib.sha1).digest()
+    o = h[19] & 15
+    otp = (struct.unpack(">I", h[o:o + 4])[0] & 0x7fffffff)
+    return otp % int(math.pow(10, key_len))
 
-except ConfigParser.Error:
+
+def test_user_pin(secret, time_window, key_len, user_pin):
+    for offset in [0, -1, -2, 1]:
+        cur_pin = get_hotp_token(secret, time_window + offset, key_len)
+        if str(cur_pin) == str(user_pin):
+            return True
+    return False
+
+try:
+    cf = configparser.ConfigParser()
+    cf.read(sProperties)
+
+    LOG_FILENAME = cf.get('LOG_DETAILS', 'LOG_FILENAME')
+
+    secret = cf.get('AUTH2', 'secret')
+    key_len = cf.get('AUTH2', 'KEY_LENGTH')
+    timeWindow = cf.get('AUTH2', 'TIME_WINDOW')
+    shell = cf.get('COMMAND', 'LOGIN_SHELL')
+
+except configparser.Error:
         sys.exit('Failed while parsing %s', sProperties)
 
 try:
-        log = logging.getLogger(LOG_FILENAME)
-        logging.basicConfig(filename=LOG_FILENAME,
-                            level=logging.INFO,
-                            format=FORMAT)
-        timeWindow = int(time.time()) / int(timeWindow)
-        key_len = float(key_len)
+    log = logging.getLogger(LOG_FILENAME)
+    logging.basicConfig(filename=LOG_FILENAME,
+                        level=logging.INFO,
+                        format=FORMAT)
+    timeWindow = int(int(time.time()) / int(timeWindow))
+    key_len = float(key_len)
 except TypeError:
-        exception_info = "Failed in setting logs, or incorrect parameters were set in properties file."
-        print(exception_info)
-        sys.exit(2)
+    exception_info = "Failed in setting logs, " + \
+        "or incorrect parameters were set in properties file."
+    print(exception_info)
+    sys.exit(2)
 
 
-# if len(sys.argv)<2:
-#    print get_hotp_token(secret, timeWindow, key_len)
-# else:
-#    user_pin = sys.argv[1]
-shell_list = shell.split(' ')
+try:
+    shell_list = shell.split(' ')
+    user_pin = getpass.getpass('Please enter 2 step authentication token : ')
 
-user_pin = getpass.getpass('Please enter 2 step authentication token : ')
-if test_user_pin(secret, timeWindow, key_len, user_pin):
-    log.info('Login successful.')
-    subprocess.call(shell_list, shell=False,)
-else:
-    log.info('Login unsuccessful. OTP Entered : ' + user_pin)
-    print("ACCESS DENIED")
-    time.sleep(2)
-    sys.exit(1)
+    if user_pin and test_user_pin(secret, timeWindow, key_len, user_pin):
+        log.info('Login successful.')
+        subprocess.call(shell_list, shell=False,)
+    else:
+        if not user_pin:
+            log.info('Login unsuccessful.Blank / Null / Empty OTP Entered.')
+        else:
+            log.info('Login unsuccessful. OTP Entered : ' + user_pin)
+
+        print("ACCESS DENIED")
+        time.sleep(2)
+        sys.exit(1)
+except Exception:
+    exception_info = 'Failed in getting userinput... Exiting as failed Login'
+    print(exception_info)
+    log.error(exception_info)
+    sys.exit(3)
